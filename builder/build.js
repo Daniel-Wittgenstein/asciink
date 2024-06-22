0af85fc31525c465a913e784bbcce8b59b67ed19
Daniel-Wittgenstein <sassaniden@gmail.com>
2024-06-22 23:13:53 +0200

    basic export works now

--- builder/build.js
+++ builder/build.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 let data = fs.readFileSync('./index.html', { encoding: 'utf8', flag: 'r' })
 const inkData = fs.readFileSync('./ink/ink-full.js', { encoding: 'utf8', flag: 'r' })
-let inkjs = "<script>window.$inkJsData = " + JSON.stringify(inkData) + "</script>"
+let inkjs = `<script>${inkData}</script>`
 data = data.replace(/\<\!\-\-DEV\-\-\>[\s\S]*?\<\!\-\-END_DEV\-\-\>/g, "")
 data = data.replace(/\<\!\-\-INJECT\-\-\>[\s\S]/, inkjs)
 const obj = {data}
