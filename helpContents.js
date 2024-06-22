d5aa498b364b1e1746d30aaef3668860e944b192
Daniel-Wittgenstein <sassaniden@gmail.com>
2024-06-23 01:11:52 +0200

    open help window

---
+++ helpContents.js
@@ -0,0 +1,23 @@
+window.helpContents = `
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HELP</title>
+    <style>
+      body {
+        margin: 0;
+        padding: 32px;
+        background: #222;
+        color: white;
+        font-family: monospace;
+        font-size: 14px;
+      }
+
+    </style>
+  </head>
+  <body>
+  ya
+  </body>
+</html>
+`
