19e7a80a3f0585293252d2238ae4cc138a6e504c
Daniel-Wittgenstein <sassaniden@gmail.com>
2024-06-25 17:30:43 +0200

    added easystar lib

--- builder/build.js
+++ builder/build.js
@@ -3,9 +3,11 @@ const fs = require('fs')
 
 let data = fs.readFileSync('./index.html', { encoding: 'utf8', flag: 'r' })
 const inkData = fs.readFileSync('./ink/ink-full.js', { encoding: 'utf8', flag: 'r' })
+const easyStarData = fs.readFileSync('./easystar/easystar-0.4.4.min.js', { encoding: 'utf8', flag: 'r' })
 let inkjs = `<script>${inkData}</script>`
+let easyStarJs = `<script>${easyStarData}</script>`
 data = data.replace(/\<\!\-\-DEV\-\-\>[\s\S]*?\<\!\-\-END_DEV\-\-\>/g, "")
-data = data.replace(/\<\!\-\-INJECT\-\-\>[\s\S]/, inkjs)
+data = data.replace(/\<\!\-\-INJECT\-\-\>[\s\S]/, inkjs + "\n\n" + easyStarJs)
 const obj = {data}
 let text = "window.$exportedHtmlData = " + JSON.stringify(obj)
 fs.writeFileSync('./auto-generated/exportHtmlData.js', text, { encoding: 'utf8'})
